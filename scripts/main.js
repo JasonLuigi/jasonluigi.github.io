0f360130591c161dced175434727b5872d8ebb85
JasonLuigi <90804822+JasonLuigi@users.noreply.github.com>
2023-04-21 13:14:52 -0400

    Basic user adding on website load

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,4 @@
 const myImage = document.querySelector("img");
-
 myImage.onclick = () => {
   const mySrc = myImage.getAttribute("src");
   if (mySrc === "images/skyrim-logo.png") {
@@ -8,4 +7,23 @@ myImage.onclick = () => {
     myImage.setAttribute("src", "images/skyrim-logo.png");
   }
 };
-
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+function setUserName() {
+    const myName = prompt("Please enter your name.");
+    if (!myName) {
+      setUserName();
+    } else {
+      localStorage.setItem("name", myName);
+      myHeading.textContent = `Introduction To The Elder Scrolls 5: Skyrim, ${myName}`;
+    }
+  }
+  if (!localStorage.getItem("name")) {
+    setUserName();
+  } else {
+    const storedName = localStorage.getItem("name");
+    myHeading.textContent = `Introduction To The Elder Scrolls 5: Skyrim, ${storedName}`;
+  }
+  myButton.onclick = () => {
+    setUserName();
+  };
